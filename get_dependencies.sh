a5f6cfe9b71fe1475a37ccbbdbcd13124b92ea9f
aperturesecurity <tatrasiel7@gmail.com>
2019-07-13 13:36:57 +0300

    updated readme

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -12,6 +12,14 @@ unzip -q SDL2_image-2.0.3.zip
 mv SDL2_image-2.0.3 SDL2_image
 rm SDL2_image-2.0.3.zip
 
+wget https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.zip
+unzip -q SDL2_mixer-2.0.4.zip
+mv SDL2_mixer-2.0.4 SDL2_mixer
+rm SDL2_mixer-2.0.4.zip
+
+
+
+
 popd
 # No need to copy SDLActivity.java et al, repo contains those from 2.0.8
 # cp external/SDL2/SDL2/android-project/app/src/main/java/org/libsdl/app/*.java android/app/src/main/java/org/libsdl/app/
